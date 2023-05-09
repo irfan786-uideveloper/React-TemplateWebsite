434825878b05a8a5a89e6800814f62fc4c4002a9
SHAIK IRFAN <75310447+irfan786-uideveloper@users.noreply.github.com>
2023-05-09 20:56:37 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from './Navbar';
+import Header from './Header';
+import Appsection from './Appsection';
+import Cards from './Cardsection';
+import Footer from './Footer';
+
+
+
+const App=()=>{
+
+    return(
+        <div>
+        <NavBar/>
+        <Header/>
+        <Appsection/>
+        <Cards/>
+        <Footer/>
+    
+
+        </div>
+    )
 }
 
 export default App;
